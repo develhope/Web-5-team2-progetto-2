75edcd8dae10caa3ed1d0ee3e79ae8833b03ce0a
Alessio Dicorato <alessio.dicorato@outlook.com>
2026-05-18 12:43:12 +0200

    creato e stilizzato componente per campi input

---
+++ guildgate/src/components/Input.tsx
@@ -0,0 +1,60 @@
+import type { InputHTMLAttributes } from "react";
+import { Lock, User, Star, Calendar, Clock, Eye, EyeOff } from "lucide-react";
+
+type InputProps = InputHTMLAttributes<HTMLInputElement> & {
+	label: string;
+	showPasswordToggle?: boolean;
+	onTogglePassword?: () => void;
+};
+
+export const Input = ({
+	label,
+	type = "text",
+	name,
+	placeholder = "",
+	value = "",
+	showPasswordToggle,
+	onTogglePassword,
+}: InputProps) => {
+	// Cambia l'icona in base al campo input
+	const getInputIcon = () => {
+		const iconClass = "h-5 w-5";
+		switch (name) {
+			case "nickname":
+				return <User className={iconClass} />;
+			case "password":
+				return <Lock className={iconClass} />;
+			case "level":
+				return <Star className={iconClass} />;
+			case "date":
+				return <Calendar className={iconClass} />;
+			case "time":
+				return <Clock className={iconClass} />;
+		}
+	};
+
+	return (
+		<div className="input-container">
+			<label className="input-label block mb-2 text-lg">{label}</label>
+			<div className="relative flex items-center">
+				<div className="input-icon-container absolute left-3 flex items-center">{getInputIcon()}</div>
+				<input
+					type={type}
+					name={name}
+					className="input-field w-full pl-10 pr-10 py-2 border rounded-md text-sm"
+					placeholder={placeholder}
+					value={value}
+				/>
+				{showPasswordToggle && (
+					<button
+						type="button"
+						onClick={onTogglePassword}
+						className="input-password-icon absolute right-3 flex items-center"
+					>
+						{type === "password" ? <Eye className="h-5 w-5" /> : <EyeOff className="h-5 w-5" />}
+					</button>
+				)}
+			</div>
+		</div>
+	);
+};
