b31c5e63b60a7b34c879d8076b982e06da153962
Nicola <meolinicola772@gmail.com>
2026-05-19 18:16:03 +0200

    nuovo

--- guildgate/src/App.tsx
+++ guildgate/src/App.tsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { Button } from "./button";
 
 export default function App() {
@@ -10,12 +10,3 @@ export default function App() {
     </>
   );
 }
-=======
-import "./App.css";
-
-function App() {
-	return <></>;
-}
-
-export default App;
->>>>>>> ebe2dec136c30cf012c2382c2ee416ea3a6565d5
